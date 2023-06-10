fba9d229b7a4013ead2db7bbc92bf92a1bf5ac35
Halryy <halryhenry2004@gmail.com>
2023-06-10 18:07:57 -0300

    0.02

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
     const $dropdown = document.querySelector(".dropdownbtn"),
-        $sidebar = document.querySelector("aside");
+    $sidebar = document.querySelector("aside");
 
+    const $sidebtns = document.querySelectorAll(".dropdowncontent > a");
+    
     const anim = [{ width: "15%" }, { width: "5%" }];
     const animMS = 100;
     let clicked = false;
     let animIsRunning = false;
 
 
-    $dropdown.addEventListener("click", () => {
+        $dropdown.addEventListener("click", () => {
+            $sidebtns.forEach((bts) => {
+                if (!clicked){
+                    bts.style.marginLeft = "50%"
+                }
+                else {
+                    bts.style.marginLeft = "-200%"
+                }
+            })
+
+        
+    
+
         
         if (!clicked) {
             clicked = true;
@@ -26,11 +40,15 @@ window.addEventListener("DOMContentLoaded", () => {
             }
             $sidebar.style.width = "5%";
         }
-    })
+        })
+
     $sidebar.addEventListener("mouseleave", () => {
         if (animIsRunning === true) return;
         clicked = false;
         animIsRunning = true;
+        $sidebtns.forEach(bts => {
+            bts.style.marginLeft = "-200%"
+        })
         $sidebar.animate(anim, { duration: animMS }).onfinish = () => {
             $sidebar.style.width = "5%";
             animIsRunning = false;
@@ -39,15 +57,26 @@ window.addEventListener("DOMContentLoaded", () => {
 
     $sidebar.addEventListener("mouseenter", () => {
         if (animIsRunning === true) return;
-        setTimeout(() => {
             if (!clicked) {
                 clicked = true
                 animIsRunning = true;
+                $sidebtns.forEach(bts => {
+                    bts.style.marginLeft = "50%"
+                })
                 $sidebar.animate(anim, { duration: animMS, direction: "reverse" }).onfinish = () => {
                     $sidebar.style.width = "15%";
                     animIsRunning = false;
                 }
             }
-        }, 250)
+    })
+
+    $sidebtns.forEach(($sidebtn) => {
+        $sidebtn.addEventListener("mouseenter", (event) => {
+                $sidebtn.children[0].classList.add("fa-fade")
+        })
+        
+        $sidebtn.addEventListener("mouseleave", (event) => {
+            $sidebtn.children[0].classList.remove("fa-fade")
+    })
     })
 })
